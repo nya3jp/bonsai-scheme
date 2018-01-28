d0efd01cde8c035f1a234a2018ec42113d2c7c46
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2018-01-28 22:46:36 +0900

    go: Implement begin.

--- go/lib/forms.go
+++ go/lib/forms.go
@@ -2,11 +2,26 @@ package lib
 
 func formQuote(env *Environment, rawArgs []Value) Value {
 	if len(rawArgs) != 1 {
-		panic("quote")
+		panic("args")
 	}
 	return rawArgs[0]
 }
 
-var formMap = map[string]func(*Environment, []Value) Value {
-	"quote": formQuote,
+func evaluateBody(env *Environment, exprs []Value) Value {
+	var result Value = TheUndef
+	for _, expr := range(exprs) {
+		result = env.Evaluate(expr)
+	}
+	return result
+}
+
+func formBegin(env *Environment, rawArgs []Value) Value {
+	return evaluateBody(env, rawArgs)
+}
+
+var formMap = make(map[string]func(*Environment, []Value) Value)
+
+func init() {
+	formMap["quote"] = formQuote
+	formMap["begin"] = formBegin
 }
